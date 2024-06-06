f30f8b1aeb26c40ad25b6c9f9da6464accc9d72a
Destin Saba <destinsaba9@gmail.com>
2024-06-06 16:01:47 -0600

    Add comments to code

--- calgary_dogs.py
+++ calgary_dogs.py
@@ -1,27 +1,27 @@
 # calgary_dogs.py
 # Destin Saba
 #
-# A terminal-based application for computing and printing statistics based on given input.
+# A terminal-based application for computing and printing calgary dog breed registration statistics based on given input.
 # Detailed specifications are provided via the Assignment 4 README file.
-# You must include the main listed below. You may add your own additional classes, functions, variables, etc. 
-# You may import any modules from the standard Python library.
-# Remember to include docstrings and comments.
+
 import pandas as pd
 import numpy as np
 
 def main():
 
-    # Import data
+    # Import data into DataFrame
     dog_breeds = pd.read_excel('CalgaryDogBreeds.xlsx')
+
+    # Create a multi-index DataFrame
     dog_breeds.set_index(['Breed','Year','Month'],inplace=True)
 
     print("ENSF 692 Dogs of Calgary")
 
-    # User input stage
+    # Get user input, continually ask until valid input is entered
     while True:
         try:
             user_input = input("Please enter a dog breed: ")
-            # Check if the input matches a school code
+            # Check if the input matches a dog breed (case insensitive)
             if (user_input.upper() in dog_breeds.index.get_level_values('Breed')):
                 breed = user_input.upper()
                 break
@@ -31,7 +31,11 @@ def main():
             print("Dog breed not found in the data. Please try again.")
 
     # Data anaylsis 
+
+    # Create index slice object
     idx = pd.IndexSlice
+
+    # Slice DataFrame to create a DataFrame with only the inputted breed's data
     dog_breed = dog_breeds.loc[idx[breed,:,:],:]
     
 
@@ -50,6 +54,7 @@ def main():
     for year in [2021, 2022, 2023]:
         yearly_total = yearly_totals.get(year,0)
 
+        # Not all dog breeds have data for each year, so only sum if there is data for a given year
         if year in years:
             breed_yearly_total = dog_breed.loc[idx[:, year, :], 'Total'].sum()
         else:
@@ -64,10 +69,15 @@ def main():
     print(f"The {breed} was {total_percentage:.6f}% of top breeds across all years.")
 
 
-    # Find and print the months that were most popular for the selected breed registrations. Print all months that tie.
+    # Find and print the months that were most popular for the selected breed registrations. Print all months that tie, in alphabetical order.
     most_popular_months = dog_breed.index.get_level_values('Month').value_counts()
     max_occurrences = most_popular_months.max()
+
+    # Use a masking operation to get most popular months 
     popular_months = most_popular_months[most_popular_months == max_occurrences].index.tolist()
+
+    # Sort alphabetically and print
+    popular_months.sort()
     print(f"Most popular month(s) for {breed} dogs: {' '.join(popular_months)}")
 
 if __name__ == '__main__':
